7850901cbc693eea168bb4f86ef7c62cad080e3b
Itminan30 <itminan30@gmail.com>
2024-06-06 00:18:36 +0600

    fpdf folder added

--- myBlogs.php
+++ myBlogs.php
@@ -29,10 +29,23 @@ require "includes/myBlogs.inc.php"
     <div class="blogs grid grid-cols-3 my-20">
         <?php foreach ($myBlogs as $blog) : ?>
             <div class="blog_card w-96 bg-base-200 m-auto ">
-                <figure><img style="height: 250px; width: 400px;" src="<?php echo $blog["MAIN_PHOTO_URL"]?>" alt="Shoes" /></figure>
+                <figure><img style="height: 250px; width: 400px;" src="<?php echo $blog["MAIN_PHOTO_URL"] ?>" alt="Shoes" /></figure>
                 <div class="card-body text-center ">
                     <h2 class="blog-card-title"><?php echo $blog["BLOG_TITLE"] ?></h2>
                     <p><?php echo $blog["PLACE_NAME"] ?></p>
+                    <?php if ($blog["APPROVAL"] === "APPROVED") : ?>
+                        <p class="text-green-500">
+                            <?php echo $blog["APPROVAL"] ?>
+                        </p>
+                    <?php elseif ($blog["APPROVAL"] === "DENIED") : ?>
+                        <p class="text-red-500">
+                            <?php echo $blog["APPROVAL"] ?>
+                        </p>
+                    <?php else : ?>
+                        <p class="text-gray-500">
+                            <?php echo $blog["APPROVAL"] ?>
+                        </p>
+                    <?php endif ?>
                     <div class="card-actions justify-end">
                         <button class="btn btn-warning mt-5 m-auto">
                             <a href="<?php echo "./blog.php?blogID={$blog['BLOG_ID']}" ?>">Explore</a>
